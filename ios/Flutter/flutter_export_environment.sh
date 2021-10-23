556b41a4c3efd89f9cd22768093e8dacedef52be
eltammo <82550049+eltammo@users.noreply.github.com>
2021-10-23 12:51:39 +0800

    Add files via upload
    commit folder

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/tamvu/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tamvu/Documents/AndroidStudioProjects/20210512_ppg"
+export "FLUTTER_TARGET=/Users/tamvu/Documents/AndroidStudioProjects/20210512_ppg/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/tamvu/Documents/AndroidStudioProjects/20210512_ppg/.dart_tool/package_config.json"
